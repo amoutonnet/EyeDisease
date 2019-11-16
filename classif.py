e362f63ec4aac4149e7c6c5298a6f9e0a2d0e56b
amoutonnet <48260266+amoutonnet@users.noreply.github.com>
2019-11-15 16:56:05 -0800

    commit

--- classif.py
+++ classif.py
@@ -5,49 +5,70 @@ from PIL import Image  # To deal with images
 import os  # To deal with directories
 from tqdm import tqdm  # For progress bar
 import sys  # To exit program
+import pickle as pkl
+import h5py
 import pandas as pd  # To deal with dataframes
 from sklearn.model_selection import StratifiedShuffleSplit  # To have evenly splitted training set and test set without unbalancing the dataset
-from imblearn.over_sampling import SMOTE  # For data augmentation
+# from imblearn.over_sampling import SMOTE  # For data augmentation
 
 
-def create_df_from_data(common_size=(512, 512)):
+def process_data(width=512, height=512, test_size=0.3):
     print('Creating a csv from data...')
-    with open('labels.json') as f:
+    with open('Datasets/labels.json') as f:
         labels = dict(json.load(f))
-    files = os.listdir('images')
-    data = []
-    for j in tqdm(files, total=len(files)):
-        im = Image.open('images/'+j)
-        im = im.resize((common_size))
-        data += [{'label': labels[j], 'image': np.array(im)}]
-
-    df = pd.DataFrame(data)
-    df.to_pickle('datasets/eyesdata.pkl')
+    files = os.listdir('Datasets/Images')
+    y = np.zeros(len(files), dtype='uint8')
+    X = np.zeros((len(files), height, width, 3), dtype='uint8')
+    for i, j in tqdm(enumerate(files), total=len(files)):
+        im = Image.open('Datasets/Images/'+j)
+        im = im.resize((width, height))
+        y[i] = labels[j]
+        X[i] = im
+    print('Splitting evenly the dataset...')
+    split = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=20)
+    for train_idx, test_idx in split.split(X, y):
+        X_train, X_test = X[train_idx], X[test_idx]
+        y_train, y_test = y[train_idx], y[test_idx]
+    print('Saving it...')
+    file = h5py.File('Datasets/data.h5', 'w')
+    file.create_dataset(
+        "X_train", np.shape(X_train), dtype='uint8', data=X_train, compression="lzf"
+    )
+    file.create_dataset(
+        "X_test", np.shape(X_test), dtype='uint8', data=X_test, compression="lzf"
+    )
+    file.create_dataset(
+        "y_train", np.shape(y_train), dtype='uint8', data=y_train, compression="lzf"
+    )
+    file.create_dataset(
+        "y_test", np.shape(y_test), dtype='uint8', data=y_test, compression="lzf"
+    )
+    file.close()
     print('Done.')
 
 
-def even_split(df, param='label', test_size=0.3):
-    split = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=20)
-    for train_idx, test_idx in split.split(df, df[param]):
-        df_train = df.loc[train_idx]
-        df_test = df.loc[test_idx]
-    df_train = df_train.reset_index(drop=True)
-    df_test = df_test.reset_index(drop=True)
-    return [df_train, df_test]
+def load_data():
+    print('Loading the data...')
+    file = h5py.File('Datasets/data.h5', 'r+')
+    X_train = np.array(file["/X_train"])
+    X_test = np.array(file["/X_test"])
+    y_train = np.array(file["/y_train"])
+    y_test = np.array(file["/y_test"])
+    file.close()
+    print('Done.')
+    return X_train, y_train, X_test, y_test
 
 
-def get_X_y(df, param='label'):
-    return df[df.columns.difference([param])], df[[param]]
+def plot_sample(X, y):
+    _, ax = plt.subplots(nrows=1, ncols=5)
+    for i in range(5):
+        ax[i].imshow(np.array(X[np.where(y == i)[0]][0]).reshape((512, 512, 3)))
+        ax[i].axis('off')
+        ax[i].set_title('Stage %d' % (i))
+    plt.show()
 
 
 if __name__ == "__main__":
-    create_df_from_data()
-    df = pd.read_pickle('datasets/eyesdata.pkl')
-    df_train, df_test = even_split(df)
-    print(df_train.groupby('label').count())
-    df_counts = df_train.groupby('label').count()
-    samp_strat = {}
-    for i in range(df_counts.shape[0]):
-        samp_strat[i] = df_counts.loc[0, 'image']-df_counts.loc[i, 'image']
-    sm = SMOTE(sampling_strategy=samp_strat, random_state=42)
-    X_res, y_res = sm.fit_resample(get_X_y(df_train))
+    # process_data()
+    X_train, y_train, X_test, y_test = load_data()
+    plot_sample(X_train, y_train)
--- readme.md
+++ readme.md
@@ -1,3 +1,74 @@
-<h1 align="center">
-    Detecting Eye Diseases Thanks to Machine Learning
-</h1>
+<h1 align="center">Detecting Eye Diseases Thanks to Machine Learning</h1>
+
+# Introduction
+
+Nowadays, data science represents a non-negligible help for physicians for them to be faster and more accurate in their task of detecting diseases. Plus, it is particularly useful in places of the world where there is no trained physicians to assess people. Eye diseases are generally hard to identify before reaching a certain level of development. Diabetic Retinopathy is one of these.  
+
+<img src="Images/DiabeticRetinopathy.png">
+
+The purpose of this project is to train different learning models on a database composed of eyes pictures, some of which are ill at different stages. Here is a sample of the database, from stage 0 (not ill) to stage 4 (proliferative diabetic retinopathy):
+
+<img src="Images/Stages.png">
+
+It seems very hard for an average person to detect the disease by looking at these images, and that is why we are going to implement models to do it for us.
+
+# The Database
+
+We were given a folder of images with precise names and a json folder contraining a list of tuples with the name of the image file as first element and the label as second element.
+The first problem is the fact that not all images are the same size. I resized them to have a 512x512 image for every image and dropped four images whose sizes were less than 512x512.  
+Secondly, if we get back to the database sample, there can be great contrast differences between images which will be an issue for our model:
+<img src="Images/DatasetContrast.png">
+Finally, we need a training set on which we will train the model and a testing set on which we will test it. We will take 70% of the dataset as the training set and 30% of it as the testing set making sure we keep the same proportion of labels in both sets.  
+The following function is doing all of this:
+
+```python
+import json  # To deal with json labels file
+import pickle as pkl # To save and load data
+def process_data(width=512, height=512, test_size=0.3):
+    with open('Datasets/labels.json') as f:
+        labels = dict(json.load(f))
+    files = os.listdir('Datasets/Images')
+    y = np.zeros(len(files), dtype='uint8')
+    X = np.zeros((len(files), height, width, 3), dtype='uint8')
+    for i, j in tqdm(enumerate(files), total=len(files)):
+        im = Image.open('Datasets/Images/'+j)
+        im = im.resize((width, height))
+        y[i] = labels[j]
+        X[i] = im
+    print('Splitting evenly the dataset...')
+    split = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=20)
+    for train_idx, test_idx in split.split(X, y):
+        X_train, X_test = X[train_idx], X[test_idx]
+        y_train, y_test = y[train_idx], y[test_idx]
+    print('Saving it...')
+    file = h5py.File('Datasets/data.h5', 'w')
+    file.create_dataset(
+        "X_train", np.shape(X_train), dtype='uint8', data=X_train, compression="lzf"
+    )
+    file.create_dataset(
+        "X_test", np.shape(X_test), dtype='uint8', data=X_test, compression="lzf"
+    )
+    file.create_dataset(
+        "y_train", np.shape(y_train), dtype='uint8', data=y_train, compression="lzf"
+    )
+    file.create_dataset(
+        "y_test", np.shape(y_test), dtype='uint8', data=y_test, compression="lzf"
+    )
+    file.close()
+```
+And to load this new processed and saved data:
+```python
+def load_data():
+    file = h5py.File('Datasets/data.h5', 'r+')
+    X_train = np.array(file["/X_train"])
+    X_test = np.array(file["/X_test"])
+    y_train = np.array(file["/y_train"])
+    y_test = np.array(file["/y_test"])
+    file.close()
+    return X_train, y_train, X_test, y_test
+```
+
+
+
+
+SVM, LASSO, CNN, FFN
